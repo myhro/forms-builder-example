571fbb230c4fb37c8fd3984d5e7090ebdf17941a
Fernando Marcio <fernandomarcio13@gmail.com>
2013-05-12 21:38:44 -0300

    Criando exibicao das entradas

--- teste/views.py
+++ teste/views.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
 
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.transaction import commit_on_success
 from django.forms.formsets import formset_factory
-from django.shortcuts import get_object_or_404, render
-from forms_builder.forms.models import Form, Field
+from django.shortcuts import get_object_or_404, redirect, render
+from forms_builder.forms.models import Form, Field, FormEntry, FieldEntry
 from teste.forms import CadastroForm, CampoForm
 
+
 def entradas(request, form_id):
-    pass
+    formentries = FormEntry.objects.filter(form_id=form_id)
+    fields = Field.objects.filter(form_id=form_id)
+    entradas = []
+    for formentry in formentries:
+        entradas.append(FieldEntry.objects.filter(entry_id=formentry.id))
+    return render(request, 'entradas.html', locals())
 
 def formulario(request, form_id):
     form = get_object_or_404(Form, id=form_id)
@@ -25,9 +33,14 @@ def novo(request):
     cadastro_form = CadastroForm(request.POST or None)
     CampoFormSet = formset_factory(CampoForm)
     campos_forms = CampoFormSet(request.POST or None)
-    if request.method == 'POST' and all([f.is_valid() for f in [cadastro_form, campos_forms]]):
-        cadastro_form.save()
-        for f in campos_forms:
-            f.instance.form = cadastro_form.instance
-            f.save()
+    if request.method == 'POST':
+        if all([f.is_valid() for f in [cadastro_form, campos_forms]]):
+            cadastro_form.save()
+            for f in campos_forms:
+                f.instance.form = cadastro_form.instance
+                f.save()
+            messages.success(request, 'Formulário salvo com sucesso.')
+            return redirect(listar)
+        else:
+            messages.error(request, 'O formulário não foi salvo.')
     return render(request, 'novo.html', locals())

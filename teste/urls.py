0c2325cd498094028dd09c5f26d00ca05727fe88
Tiago Ilieve <tiago.myhro@gmail.com>
2013-05-13 00:22:39 -0300

    Refatorando urls.py

--- teste/urls.py
+++ teste/urls.py
@@ -7,20 +7,22 @@ import forms_builder.forms.urls
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'teste.views.home', name='home'),
-    # url(r'^teste/', include('teste.foo.urls')),
-    url(r'^$', 'teste.views.home', name='home'),
-    url(r'^criar/', 'teste.views.criar_editar_form', name='criar'),
-    url(r'^editar/(?P<form_id>\d+)/', 'teste.views.criar_editar_form', name='editar'),
-    url(r'^entradas/(?P<form_id>\d+)/', 'teste.views.entradas', name='entradas'),
-    url(r'^formulario/(?P<form_id>\d+)/', 'teste.views.formulario', name='formulario'),
-    url(r'^listar/', 'teste.views.listar', name='listar'),
-
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^forms/', include(forms_builder.forms.urls)),
+)
+
+urlpatterns += patterns('teste.views',
+    # Examples:
+    # url(r'^$', 'teste.views.home', name='home'),
+    # url(r'^teste/', include('teste.foo.urls')),
+    url(r'^$', 'home', name='home'),
+    url(r'^criar/', 'criar_editar_form', name='criar'),
+    url(r'^editar/(?P<form_id>\d+)/', 'criar_editar_form', name='editar'),
+    url(r'^entradas/(?P<form_id>\d+)/', 'entradas', name='entradas'),
+    url(r'^formulario/(?P<form_id>\d+)/', 'formulario', name='formulario'),
+    url(r'^listar/', 'listar', name='listar'),
 )
